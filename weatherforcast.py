7a983d8f8b3ff505f4df2593f39794f524c1c2af
BoginskiyVV <vov4ik84@gmail.com>
2022-12-15 01:52:26 +0300

    update file

--- weatherforcast.py
+++ weatherforcast.py
@@ -3,13 +3,14 @@ from datetime import datetime
 from pprint import pprint
 from config import open_weather_token
 
+
 def get_weather(city, open_weather_token):
     try:
         r = requests.get(
             f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric'
             )
         data = r.json()
-        # pprint(data)
+        pprint(data)
         
         city = data['name']
         cur_weather = data['main']['temp']
