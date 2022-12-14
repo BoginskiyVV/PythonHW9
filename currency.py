21bc7448425e4efdb7574115eb05691d02ff7dac
BoginskiyVV <vov4ik84@gmail.com>
2022-12-14 21:04:17 +0300

    update get_currency file

--- currency.py
+++ currency.py
@@ -4,35 +4,22 @@ from pprint import pprint
 from datetime import datetime
 
 
-# def get_currency_list():
-#
-#     cur = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-#     list_cur = cur.json()
-#     pprint(list_cur)
-#     return list_cur
-#
-#
-# get_currency_list()
-
 def get_currency():
     user_request = input('Введите буквенный код валюты EUR, GBP или USD: ').upper()
     cur = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
     if user_request == 'EUR':
-        # cur = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
         list_cur = cur.json()
         cur = list_cur['Valute']['EUR']['Name']
         value = list_cur['Valute']['EUR']['Value']
         print(f'Курс ЦБ РФ на {datetime.now()}\n'
               f'1 {cur} - {value} российских рублей')
     elif user_request == 'GBP':
-        # cur = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
         list_cur = cur.json()
         cur = list_cur['Valute']['GBP']['Name']
         value = list_cur['Valute']['GBP']['Value']
         print(f'Курс ЦБ РФ на {datetime.now()}\n'
               f'1 {cur} - {value} российских рублей')
     elif user_request == 'USD':
-        # cur = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
         list_cur = cur.json()
         cur = list_cur['Valute']['USD']['Name']
         value = list_cur['Valute']['USD']['Value']
@@ -40,19 +27,6 @@ def get_currency():
               f'1 {cur} - {value} российских рублей')
     else:
         print('Утоните курс какой валюты вас интересует')
-# return list_cur
 
 
 get_currency()
-
-
-
-
-# # 'EUR' 'GBP' 'USD'
-#
-#
-# get_currency_list()
-#
-#
-# def get_currency(list_cur):
-# get_currency(list_cur)
